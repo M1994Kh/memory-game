549352172a59b3d178468238a868d2da4789281e
Mehran Khorasani <77451572+M1994Kh@users.noreply.github.com>
2023-03-23 22:45:37 +0330

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function art1() {
+    window.open("aron.html");
+}
+function art2()   {
+    window.open("ebi.html")
+}
+function art3()   {
+    window.open("hengameh.html")
+}
+function art4()   {
+    window.open("moein.html")
+}
+function art5()   {
+    window.open("shadmehr.html")
+}
+function art6()   {
+    window.open("siavash.html")
+}
