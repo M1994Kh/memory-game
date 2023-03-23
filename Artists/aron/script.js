671ad2a7cad63f42e3ace7e73508cee9f0315b0d
Mehran Khorasani <77451572+M1994Kh@users.noreply.github.com>
2023-03-23 22:49:31 +0330

    Add files via upload

---
+++ Artists/aron/script.js
@@ -0,0 +1,11 @@
+var x = document.getElementById("audio1");
+var y = document.getElementById("audio2");
+function play1() {
+  x.play();
+}
+function play2() {
+    y.play();
+}
+function rtrn()    {
+    
+}
